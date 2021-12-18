9b8bb12c19bd063a146b26ed8e5a29311529d8ac
김대영 <57317043+daeyeong1573@users.noreply.github.com>
2021-12-18 22:33:39 +0900

    Update XintervalN.kt

--- app/src/main/java/com/example/programers/XintervalN.kt
+++ app/src/main/java/com/example/programers/XintervalN.kt
@@ -6,13 +6,13 @@ fun main(){
     var input = readLine()!!.split(' ').map { it.toInt() }
     solution(input[0],input[1]).forEach { print(it) }
 }
-//
-//fun solution(x: Int, n: Int): Array<Long?> {
-//    var answer = arrayOfNulls<Long>(n)
-//    var temp : Long = 0
-//    for (i in 0 until n){
-//        temp += x
-//        answer[i] = temp
-//    }
-//    return answer
-//}
+
+fun solution(x: Int, n: Int): Array<Long?> {
+   var answer = arrayOfNulls<Long>(n)
+   var temp : Long = 0
+   for (i in 0 until n){
+       temp += x
+       answer[i] = temp
+   }
+   return answer
+}
