7895094f1e27a5918bcb3b94f4dea4536ff56faa
KDY <rlaeodud9983@naver.com>
2022-03-28 21:00:59 +0900

    프로그래머스 Level 2 피보나치

---
+++ app/src/main/java/com/example/programers/level2/Fibonacci.kt
@@ -0,0 +1,16 @@
+package com.example.programers.level2
+
+// 프로그래머스 LEVEL 2 피보나치 수열
+
+fun main(){
+    print(solution(3))
+}
+
+fun solution(n : Int): Int {
+    // n+1 크기의 배열을 1로 초기화
+    var answer = IntArray(n + 1){ 1 }
+    // 3부터 n까지 반복하며 피보나치 수를 배열에 저장해 줍니다.
+    (3..n).forEach{ answer[it] = (answer[it - 2] + answer[it - 1]) % 1234567 }
+    // 배열에 n번 칸에 들어있는 숫자를 반환합니다.
+    return answer[n]
+}
